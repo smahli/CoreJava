cb7ef0fb6af729299d8fff01c02e96381df5524f
smahli <mahlishiwshankar@gmail.com>
2021-09-13 21:37:28 -0700

    initial commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    // If we don't create a Constructor here then Compiler automatically creates a Constructor that is known as Default constructor
+
+public static void main(String[] args) {
+Test t = new Test();
+}
+}
