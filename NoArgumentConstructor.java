3362efed7fdfcc85f62dcd1f68dccd1a301112f0
smahli <mahlishiwshankar@gmail.com>
2021-09-13 21:53:30 -0700

    basics

---
+++ NoArgumentConstructor.java
@@ -0,0 +1,13 @@
+public class NoArgumentConstructor {
+
+    NoArgumentConstructor(){
+        System.out.println("No argument Constructor");
+
+    }
+
+    // called No argument Constructor
+    public static void main(String[] args) {
+        NoArgumentConstructor  n = new NoArgumentConstructor();
+
+    }
+}
