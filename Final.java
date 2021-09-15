39c5649afd71d15626e5e4daaf39503e614a7cdd
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-09-15 03:59:34 -0700

    final class

--- Final.java
+++ Final.java
@@ -1,5 +1,11 @@
-public class Final {
-    public static void main(String[] args) {
-
+final class Final {
+    void show() {
+        System.out.println("Welcome to final class");
     }
+}
+class Final2 extends Final{  // extned final class cannot be inherited because of the final keyword
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world of java");
+    }
+
 }
