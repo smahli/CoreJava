c452ee43a321409d53f3facb478b3bc56c0ef0ec
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-09-16 01:47:07 -0700

    how try catch works

--- TryCatch.java
+++ TryCatch.java
@@ -1,2 +1,29 @@
 public class TryCatch {
+    public static void main(String[] args) {
+        System.out.println("1");
+        try {
+            System.out.println("2");
+
+             int a = 100;
+             int b=0;
+             int c;
+            System.out.println("3");
+
+             c=a/b;
+            System.out.println("4");
+
+            System.out.println(c);
+            System.out.println("5");
+
+        }
+         catch (ArithmeticException e){
+             System.out.println("6");
+
+             System.out.println(e);
+             System.out.println("7");
+
+         }
+        System.out.println("hello");
+
+    }
 }
