46d358a7b835d785ea2d8ad7ff9f69461b32d621
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-09-15 22:58:06 -0700

    just a basic example

---
+++ ExceptionExample.java
@@ -0,0 +1,21 @@
+public class ExceptionExample {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");  // It will print till 7 then it will throw an exception / by zero
+        System.out.println(8/0);   // exception is unwanted event that disrupts the normal flow of control
+        System.out.println("9");
+        System.out.println("10");
+        System.out.println("11");
+        System.out.println("12");
+        System.out.println("13");
+        System.out.println("14");
+        System.out.println("15");
+
+    }
+}
