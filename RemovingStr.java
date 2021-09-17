131effa40cd95baae1fd35bd153eae5ef2ac0a5b
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-09-16 23:26:13 -0700

    updated

--- RemovingStr.java
+++ RemovingStr.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class RemovingStr {
+
+        public static void main(String[] args) {
+            String  str1;
+            String str2;
+            System.out.println(" Enter First String");
+            Scanner sc = new Scanner(System.in);
+            str1 = sc.nextLine();
+            System.out.println("Enter Second String");
+            str2= sc.nextLine();
+
+        }
+
+
+
+
 }
