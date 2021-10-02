d2ddb0d718926ac9a538b8843fd6b0ab02253475
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-10-01 22:06:04 -0700

    updated

--- ReverseString.java
+++ ReverseString.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class ReverseString {
+
+    public static void main(String[] args) {
+        String str1;
+        String rev="";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string that you want to reverse");
+        str1= sc.nextLine();
+        for (int i =str1.length()-1;i>=0;i--){
+            rev=rev+str1.charAt(i);
+        }
+
+        System.out.println(" The Reversed String is "+rev);
+
+
+    }
 }
