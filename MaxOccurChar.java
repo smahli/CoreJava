67ec64ac7700edff2c9bcff4c509db46b05ad56e
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-10-02 19:18:22 -0700

    some changes required

--- MaxOccurChar.java
+++ MaxOccurChar.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class MaxOccurChar {
+
+    public static void main(String[] args) {
+        String Str1;
+        int[] array = new int[127];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string to check Occurrence of numbers of characters");
+        Str1= sc.nextLine();
+        Str1=Str1.replaceAll("\\s","");
+        System.out.println(Str1);
+
+        for (int i=0;i<Str1.length();i++){
+            array[Str1.charAt(i)]= array[Str1.charAt(i)]+1;
+        }
+        int max =-1;
+        char c=' ';
+        for(int i=0;i<Str1.length();i++) {
+            if (max<array[Str1.charAt(i)]){
+                max = Str1.charAt(i);
+                c = Str1.charAt(i);
+            }
+        }
+
+        System.out.println("Maximum Repeated Character:  "+c);
+    }
+
+
 }
+
