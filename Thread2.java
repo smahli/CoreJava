79b7c35d060f0f8dc78f9368eb15346ff1390946
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-10-05 19:40:29 -0700

    updated

--- Thread2.java
+++ Thread2.java
@@ -5,7 +5,8 @@ public class Thread2 extends Thread {
     }
 
     public static void main(String[] args) {
-        System.out.println("Main Thread");
+        //  System.out.println("Main Thread");
+      //  Thread.currentThread().setDaemon(true);  // This will throw an exception because main Thread Can not be set to daemon Thread
         Thread2 t2 = new Thread2();
         t2.setDaemon(true); // make sure declare daemon before start()   opposite will throw exception
         t2.start();
