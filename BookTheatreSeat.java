1afd055a3df5a761248c37f263ca20155c24deaf
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-10-09 22:10:40 -0700

    Example of Synchronize block

--- BookTheatreSeat.java
+++ BookTheatreSeat.java
@@ -1,20 +1,36 @@
 import java.lang.Thread;
 public class BookTheatreSeat {
     int totalSeat =10;
-     synchronized void bookSeat(int seat){
-        if (totalSeat>=seat){
-            System.out.println(Thread.currentThread().getName()+ " Seats Booked Successfully");
-            totalSeat=totalSeat-seat;
-            System.out.println(totalSeat+" Seats left");
+     /*synchronized*/ void bookSeat(int seat){                // we can use synchronized method to synchronize threads
+                                                        // It will consume a lot of time to execute if we have 1000 lines of codes
+                                                        // So It will be better if we use Synchronize block used here
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
 
+        synchronized (this){                        // This is an example of Synchronize block  only this code has to executed by different thread separately
+
+             if (totalSeat>=seat){
+                 System.out.println(Thread.currentThread().getName()+ " Seats Booked Successfully");
+                 totalSeat=totalSeat-seat;
+                 System.out.println(totalSeat+" Seats left");
+            }
+
+             else{
+
+                 System.out.println(Thread.currentThread().getName()+" Seat Cannot be booked");
+                 System.out.println(totalSeat+" Seats Left");
+             }
         }
-
-        else{
-
-            System.out.println(Thread.currentThread().getName()+" Seat Cannot be booked");
-            System.out.println(totalSeat+" Seats Left");
-        }
-
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
+         System.out.println(Thread.currentThread().getName());
     }
 }
 class MovieBookAPP extends Thread{
