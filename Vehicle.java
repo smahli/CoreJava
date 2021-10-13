7504ae0743c7cc7c483978851f23f48465ad63fc
Shiwshankar Mahli <mahlishiwshankar@gmail.com>
2021-10-12 22:21:55 -0700

    Updated

--- Vehicle.java
+++ Vehicle.java
@@ -1,4 +1,29 @@
-abstract class Vehicle{  // Object creation of abstract class is not possible.
+
+public abstract class Vehicle {
     int no_of_Tyres;
-    abstract void Start();
+
+    abstract void start();
 }
+class Car extends Vehicle{
+    @Override
+    void start() {
+        System.out.println(" Car Starts with Key");
+    }
+}
+
+class Scooter extends Vehicle{
+    @Override
+    void start() {
+        System.out.println(" Scooter starts with kick");
+    }
+
+    public static void main(String[] args) {
+        //Vehicle v= new Vehicle();    // Why because Vehicle is abstract it cannot be instantiated mean we cannot create object of abstract class
+        Car c = new Car();
+        Scooter s = new Scooter();
+        c.start();
+        s.start();
+    }
+}
+
+
